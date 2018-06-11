250120281d092c6c4f4a1970132e354766ac635e
Nazario A. Ayala <nazario@niztech.com>
2018-06-10 19:05:50 -0700

    Added some config stuff

--- views/config.php
+++ views/config.php
@@ -8,9 +8,10 @@
 ?>
 
 <main class="niztech-youtube-container">
-    <div class="niztech-youtube-mast">
-        <img class="niztech-youtube-masthead__logo"
+    <div class="niztech-youtube-section niztech-youtube-mast">
+        <img class="niztech-youtube-logo"
              src="<?php echo esc_url( plugins_url( '../_inc/niztech_logo.svg', __FILE__ ) ); ?>" alt="Niztech"/>
+        <span class="niztech-title">Niztech YouTube</span>
     </div>
 
     <section class="niztech-youtube-section">
@@ -18,6 +19,7 @@
             <label class="niztech-youtube-label" for="niztech-youtube-api">YouTube V3 API Key</label>
             <input class="niztech-youtube-input" id="niztech-youtube-api"
                    value="<?php echo Niztech_Youtube::get_youtube_api_key(); ?>"/>
+            <button class="niztech-youtube-btn niztech-youtube-btn-primary">Save</button>
         </div>
     </section>
 
