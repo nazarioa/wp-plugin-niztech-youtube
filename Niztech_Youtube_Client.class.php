6c3b6506342765c39a775118fcd3e8599d679db8
Nazario A. Ayala <nazario@niztech.com>
2018-06-29 20:35:49 -0700

    Fixed edge case where no data has been saved

--- Niztech_Youtube_Client.class.php
+++ Niztech_Youtube_Client.class.php
@@ -19,12 +19,17 @@ class Niztech_Youtube_Client {
 		$type               = Niztech_Youtube::video_source_get_meta( Niztech_Youtube::PLUGIN_PREFIX . 'type' );
 		$foreign_key_object = Niztech_Youtube::get_video_or_playlist_code_and_foreign_key( $type, $post_id );
 
+		if ( empty( $foreign_key_object ) || empty( $type ) ) {
+			return null;
+		}
+
 		if ( $type == Niztech_Youtube::TYPE_OPTION_PLAYLIST ) {
-			return $wpdb->get_results( 'SELECT * FROM ' . $wpdb->prefix . Niztech_Youtube::TBL_VIDEOS );//.
-//			                           " WHERE playlist_id = $foreign_key_object->id" );
+
+			return $wpdb->get_results( 'SELECT * FROM ' . $wpdb->prefix . Niztech_Youtube::TBL_VIDEOS .
+			                           " WHERE playlist_id = $foreign_key_object->id;" );
 		} elseif ( $type == Niztech_Youtube::TYPE_OPTION_VIDEO ) {
 			return $wpdb->get_results( 'SELECT * FROM ' . $wpdb->prefix . Niztech_Youtube::TBL_VIDEOS .
-			                           " WHERE post_id = $foreign_key_object->post_id AND playlist_id = 0" );
+			                           " WHERE post_id = $foreign_key_object->post_id AND playlist_id = 0;" );
 		}
 
 	}
