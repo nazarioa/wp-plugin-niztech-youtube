79f4cd0b39de796783c1c9fe12d46e81e7ed3056
Nazario A. Ayala <nazario@niztech.com>
2018-09-09 14:58:50 -0700

    Added first test file Niztech_YoutubeTest.php

---
+++ tests/Niztech_YoutubeTest.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nazario
+ * Date: 9/9/18
+ * Time: 2:55 PM
+ */
+
+/**
+ * Class Niztech_YoutubeTest
+ *
+ * @package Niztech_Youtube
+ */
+class Niztech_YoutubeTest extends WP_UnitTestCase {
+	public function setUp() {
+		parent::setUp();
+
+		$this->class_instance = new Niztech_Youtube();
+	}
+
+	public function testV2_delete_video_by_post_playlist() {
+
+	}
+
+	public function testInit_hooks() {
+
+	}
+
+	public function testIs_youtube_url() {
+
+	}
+
+	public function testGet_local_playlist_row() {
+
+	}
+
+	public function testGet_video_info_for() {
+
+	}
+
+	public function testCommit_playlist_data_to_wp() {
+
+	}
+
+	public function testPlugin_deactivation() {
+
+	}
+
+	public function testCreate_empty_local_playlist_row() {
+
+	}
+
+	public function testExtract_youtube_code() {
+
+	}
+
+	public function testVideo_source_get_meta() {
+
+	}
+
+	public function testQuery_video_data_from_youtube() {
+
+	}
+
+	public function testInit() {
+
+	}
+
+	public function testQuery_playlist_data_from_youtube() {
+
+	}
+
+	public function testV2_delete_video_by_id() {
+
+	}
+
+	public function testEnter_api_key() {
+
+	}
+
+	public function testSet_youtube_api_key() {
+
+	}
+
+	public function testCommit_video_data_to_wp() {
+
+	}
+
+	public function testV2_delete_playlist_by_id() {
+
+	}
+
+	public function testV2_delete_playlist_by_post_id() {
+
+	}
+
+	public function testSetup_youtube_google_client() {
+
+	}
+
+	public function testGet_video_or_playlist_code_and_foreign_key() {
+
+	}
+
+	public function testCreate_table_video() {
+
+	}
+
+	public function testGet_playlist_info_for() {
+
+	}
+
+	public function testPlugin_activation() {
+
+	}
+
+	public function testProcess_Google_Service_YouTube_ThumbnailDetails() {
+
+	}
+
+	public function testCreate_table_playlist() {
+
+	}
+
+	public function testGet_youtube_api_key() {
+
+	}
+
+	public function testVerify_key() {
+
+	}
+}
