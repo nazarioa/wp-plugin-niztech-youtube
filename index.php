cd620417d511820f0522103de5a19d29228f03e1
Nazario A. Ayala <nazario@niztech.com>
2025-11-15 15:49:35 -0800

    Updated main index.php

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: nazario
- * Date: 6/10/18
- * Time: 3:43 PM
+ * Niztech YouTube
+ *
+ * @package     Niztech_Youtube
+ * @author      Nazario A. Ayala
+ * @copyright   Niztech
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ *  Plugin Name: Niztech Youtube
+ *  Plugin URI: https://github.com/nazarioa/wp-plugin-niztech-youtube
+ *  Description: Plugin that allows vidoes posted on youtube to easily be used in a WP site as a library.
+ *  Author: Nazario A. Ayala
+ *  Version: 1.0.1
  */
-// Silence!
